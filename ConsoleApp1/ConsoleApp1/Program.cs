7108b0775bea34bdf021b00f84934dcdc8f7b082
Dmitriy Zastavnuyk <zastavnyk95@gmail.com>
2017-07-01 00:17:59 +0300

    sourse code of the task with registers 01/07/17

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string ProcessInput(string s)
+        {
+            //String[] substrings = s.Split('&');
+            //foreach (var substring in substrings)
+            //    Console.WriteLine(substring
+            // TODO Verify and validate the input 
+            // string as appropriate for your application.
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("s1");
+            //Console.ReadLine();
+            //string s1 = "evd";
+            //Console.WriteLine("s2");
+            //string s2 = "d";
+            //Console.ReadLine();
+            //Program.WriteToRegister(s1, s2);
+            //Console.ReadLine();
+
+            Console.WriteLine("Alert.exe invoked with the following parameters.\r\n");
+            Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+
+            Console.WriteLine("\n\nArguments:\n");
+            char[] ch = { '&',':','?'};
+            foreach (string s in args)
+            {
+                String[] substrings = s.Split(ch);
+                for (var i = 0; i < substrings.Length; i++) { Console.WriteLine(substrings[i]); }
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+
+        }
+
+        //static public void WriteToRegister(string uriSchemaName, string path)
+        //{
+        //    try
+        //    {
+
+        //        RegistryKey currentUserKey = Registry.ClassesRoot;
+        //        RegistryKey subKey1 = currentUserKey.CreateSubKey(uriSchemaName,true);
+
+        //        RegistryKey subKey11 = subKey1.CreateSubKey("DefaultIcon", true);
+        //        subKey11.SetValue("(Default)", "C:\\Users\\Dima\\Documents\\Visual Studio 2017\\Projects\\TestApp\\TestApp\\bin\\Debug\\TeatApp.exe"+","+"1");
+        //        RegistryKey subKey12 = subKey1.CreateSubKey("shell", true);
+        //        RegistryKey subKey2 = subKey12.CreateSubKey("open", true);
+
+        //        RegistryKey subKey3 = subKey2.CreateSubKey("command", true);
+        //        subKey3.SetValue("По умолчанию" , "C:\\Users\\Dima\\Documents\\Visual Studio 2017\\Projects\\TestApp\\TestApp\\bin\\Debug\\TestApp.exe" + "" + " \"%1\"");
+
+
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Error" + ex.Message);
+        //    }
+    //}
+    }
+}
